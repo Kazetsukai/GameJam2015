d06430b4adf55c414f8738872697ee8658585ab1
joejimufu <righteouslyalgebraic@gmail.com>
2015-01-25 10:29:34 +1300

    cworot

---
+++ Hystorical/Assets/Levels/UFO/Scripts/UFOSpin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOSpin : MonoBehaviour {
+
+	public float UFOSpinRate = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localEulerAngles += new Vector3(0, UFOSpinRate * Time.deltaTime, 0);
+	}
+}
