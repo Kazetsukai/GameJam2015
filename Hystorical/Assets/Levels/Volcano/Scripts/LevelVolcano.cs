d2e4c5d1db8b64cc26c8a26db3b5ed089ca0201a
joejimufu <righteouslyalgebraic@gmail.com>
2015-01-24 15:54:12 +1300

    asdsa
    asdasd

--- Hystorical/Assets/Levels/Volcano/Scripts/LevelVolcano.cs
+++ Hystorical/Assets/Levels/Volcano/Scripts/LevelVolcano.cs
@@ -5,18 +5,18 @@ using System.Collections.Generic;
 public class LevelVolcano : MonoBehaviour {
 
 	public GameObject worldCenterObject;
-	public GameObject LevelObjectsParentEmpty;
-	public GameObject objectDespawner;
+	private GameObject LevelObjectsParent;
+	public GameObject objectDespawnerPrefab;
 
+	public GameObject lavaPrefab;
 	public GameObject floorPrefab;
 	public GameObject boulderPrefab;
 	public GameObject treePrefab;
 	public GameObject plantPrefab;
 	public GameObject rockPrefab;
 
-	public float levelWidth = 17f;
+	public float levelWidth = 32f;
 
-	private float floorSpawnRate = 1f;
 	public float boulderSpawnRate = 1f;
 	public float treeSpawnRate = 1f;
 	public float plantSpawnRate = 1f;
@@ -24,7 +24,6 @@ public class LevelVolcano : MonoBehaviour {
 
 	public float spawnTime = 5f;
 
-	private float floorSpawnTimer = 0f;
 	public float boulderSpawnTimer = 0f;
 	public float treeSpawnTimer = 0f;
 	public float plantSpawnTimer = 0f;
@@ -32,37 +31,52 @@ public class LevelVolcano : MonoBehaviour {
 
 	public float spawnDistance = 40f; //distance objects spawn from worldCenterObject
 
-	private int floorCount = 0;
+	public int numSpawned = 0;
+
+	GameObject lava;
+	Vector3 lavaOffset;
+
+	GameObject objectDespawner;
+	float objectDespawnerDistance = 80f;
 
 	// Use this for initialization
 	void Start () 
 	{
+		//spawn lava thing
+		LevelObjectsParent = new GameObject ("LevelObjects");
+		LevelObjectsParent.transform.parent = this.transform;
+
+		objectDespawner = (GameObject)Instantiate (objectDespawnerPrefab);
+
+
+		lava = (GameObject)Instantiate (lavaPrefab);
+		lava.transform.position = new Vector3 (-21.38f, 0.34f, -4.11f);
+		lavaOffset = worldCenterObject.transform.position - lava.transform.position;
+		lava.transform.parent = LevelObjectsParent.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{	
+		//Update positions
+		objectDespawner.transform.position = worldCenterObject.transform.position - new Vector3(objectDespawnerDistance, 0 , 0);
+		lava.transform.position = worldCenterObject.transform.position - lavaOffset;
+
 		//update timers
-		floorSpawnTimer += Time.fixedDeltaTime * floorSpawnRate;
 		boulderSpawnTimer += Time.fixedDeltaTime * boulderSpawnRate;
 		treeSpawnTimer += Time.fixedDeltaTime * treeSpawnRate;
 		plantSpawnTimer += Time.fixedDeltaTime * plantSpawnRate;
 		rockSpawnTimer += Time.fixedDeltaTime * rockSpawnRate;
 
 		//Spawn floor objects as worldCenterObject moves
-		if (floorSpawnTimer >= spawnTime)
-		{ 
-			GameObject newFloor = (GameObject)Instantiate(floorPrefab);
-			int something = worldCenterObject.transform.position.x / 16f;
-
-
-			newFloor.transform.position = new Vector3(floorCount * 32,0, 0);
-			floorCount++;
-			//newFloor.transform.parent = LevelObjectsParentEmpty.transform;// DON'T DO THIS!! Makes floor slide along map...	
-
-			//reset spawnTimer
-			floorSpawnTimer = 0;
+		int numToSpawn = (int)(worldCenterObject.transform.position.x / 32f) + 5;
+		for (int i = numSpawned; i < numToSpawn; i++) 
+		{
+			GameObject newFloor = (GameObject)Instantiate (floorPrefab);
+			newFloor.transform.position = new Vector3(i * 32f,0, 0);
+			newFloor.transform.parent = LevelObjectsParent.transform;
 		}
+		numSpawned = numToSpawn;	
 
 		//Spawn everything else
 		if (boulderSpawnTimer >= spawnTime)
@@ -71,7 +85,7 @@ public class LevelVolcano : MonoBehaviour {
 			newBoulder.transform.position = worldCenterObject.transform.position + new Vector3(spawnDistance,0, Random.Range(-levelWidth / 2, levelWidth / 2));
 			newBoulder.transform.localEulerAngles = new Vector3(newBoulder.transform.localEulerAngles.x, Random.Range(0, Mathf.PI * 2), newBoulder.transform.localEulerAngles.z);
 			newBoulder.transform.localScale = new Vector3(1, 1, 1) * Random.Range(1f, 4f);
-			newBoulder.transform.parent = LevelObjectsParentEmpty.transform;
+			newBoulder.transform.parent = LevelObjectsParent.transform;
 
 			//reset spawnTimer
 			boulderSpawnTimer = 0;
@@ -83,7 +97,7 @@ public class LevelVolcano : MonoBehaviour {
 			newPlant.transform.position = worldCenterObject.transform.position + new Vector3(spawnDistance,0, Random.Range(-levelWidth / 2, levelWidth / 2));
 			newPlant.transform.localEulerAngles = new Vector3(newPlant.transform.localEulerAngles.x, Random.Range(0, Mathf.PI * 2), newPlant.transform.localEulerAngles.z);
             newPlant.transform.localScale = new Vector3(1, 1, 1) * Random.Range(1f, 4f);
-			newPlant.transform.parent = LevelObjectsParentEmpty.transform;
+			newPlant.transform.parent = LevelObjectsParent.transform;
 
 			//reset spawnTimer
 			plantSpawnTimer = 0;
@@ -94,7 +108,7 @@ public class LevelVolcano : MonoBehaviour {
 			newTree.transform.position = worldCenterObject.transform.position + new Vector3(spawnDistance,0, Random.Range(-levelWidth / 2, levelWidth / 2));
 			newTree.transform.localEulerAngles = new Vector3(newTree.transform.localEulerAngles.x, Random.Range(0, Mathf.PI * 2), newTree.transform.localEulerAngles.z);
 			newTree.transform.localScale = new Vector3(1, 1, 1) * Random.Range(1f, 4f);
-			newTree.transform.parent = LevelObjectsParentEmpty.transform;
+			newTree.transform.parent = LevelObjectsParent.transform;
 
 			//reset spawnTimer
 			treeSpawnTimer = 0;
@@ -104,7 +118,7 @@ public class LevelVolcano : MonoBehaviour {
 		{
 			GameObject newRock = (GameObject)Instantiate(rockPrefab, worldCenterObject.transform.position + new Vector3(Random.Range (-20f, 20f),30, Random.Range(-levelWidth / 2, levelWidth / 2)), Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up));
 			newRock.transform.localScale = new Vector3(1, 1, 1) * Random.Range(1f, 4f);
-			newRock.transform.parent = LevelObjectsParentEmpty.transform;
+			newRock.transform.parent = LevelObjectsParent.transform;
 
 			//reset spawnTimer
 			rockSpawnTimer = 0;
