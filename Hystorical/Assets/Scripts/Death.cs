3758f5ceceb91e1eeb95dc49d5beb92c94dd59d8
Declan <declantvb@gmail.com>
2015-01-24 13:34:43 +1300

    Fire effects and dying

---
+++ Hystorical/Assets/Scripts/Death.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+	public int DieTimer = -1;
+	public int yougondie = 0;
+	
+	// Use this for initialization
+	void Start () {
+		yougondie = Random.Range(5,10);
+	}
+	
+	void FixedUpdate () {
+		if (Time.time > yougondie && DieTimer < 0) 
+		{
+			DieByFire();
+		}
+		
+		if (DieTimer >= 0) 
+		{
+			DieTimer--;
+		}
+		
+		if (DieTimer == 0) 
+		{
+			Die();
+		}
+	}
+	
+	public void DieByFire()
+	{
+		var fireEmitter = transform.FindChild("Fire");
+		if (fireEmitter != null) 
+		{
+			fireEmitter.gameObject.SetActive(true);
+			DieTimer = Random.Range(300, 600);
+		}
+	}
+	
+	public void Die()
+	{
+		transform.gameObject.SetActive(false);
+	}
+}
