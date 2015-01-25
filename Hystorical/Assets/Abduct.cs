2061b12aed10133e2b1fbbee4c92f9774e43f5a4
Declan <declantvb@gmail.com>
2015-01-25 14:46:58 +1300

    ufo ai

---
+++ Hystorical/Assets/Abduct.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Abduct : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider) 
+	{
+		var killer = collider.gameObject.GetComponent<Death>();
+		if (killer != null) 
+		{
+			killer.DieByElectricity();
+		}
+	}
+}
--- Hystorical/Assets/Scripts/UFOController.cs
+++ Hystorical/Assets/Scripts/UFOController.cs
@@ -7,23 +7,42 @@ public class UFOController : MovementController {
 	
 	private IEnumerable<GameObject> targets;
 	public GameObject currentTarget = null;
+	public float TractorFactor = 1;
+	public float TractorRange = 2;
 	
 	protected override void Init()
 	{
 		var allObj = GameObject.FindObjectsOfType<GameObject>();
 		
 		targets = allObj.Where(o=>o.layer == LayerMask.NameToLayer("Bounded"));
+		
+		LookForwards = false;
 	}
 	
-	protected override void SetupTarget()
+	protected override void DoLogic()
 	{
 		if (currentTarget == null) 
 		{
-			var people = targets.Where(t=>t.name == "Player" || t.name == "NPC");
+			var people = targets.Where(t=>t.name.StartsWith("Player") || t.name.StartsWith("NPC"));
 			int index = Random.Range(0, people.Count());
 			currentTarget = people.ElementAt(index);
-		}
+        }
+        
+		var xzPosTarget = currentTarget.transform.position;		
+		var xzPos = transform.position;
+		xzPosTarget.y=0;
+		xzPos.y=0;
 		
+		if ((xzPos - xzPosTarget).magnitude < TractorRange) 
+		{
+			var oldpos=currentTarget.transform.position;
+			currentTarget.transform.position = new Vector3(oldpos.x,oldpos.y + 10f * Time.fixedDeltaTime,oldpos.z);
+			currentTarget.rigidbody.AddForce((xzPos - xzPosTarget).normalized * TractorFactor, ForceMode.VelocityChange);
+		}
+    }
+    
+    protected override void SetupTarget()
+	{		
 		var centerMarker = GameObject.Find("WorldCenterMarker");
 		
 		_target = (currentTarget.transform.position) - transform.position;
@@ -33,7 +52,10 @@ public class UFOController : MovementController {
 	{
 		if (currentTarget == null || currentTarget.name != "Cow") 
 		{
-			currentTarget = collider.gameObject;
+			if (new string[]{"Player", "Cow", "NPC"}.Contains(collider.gameObject.name)) 
+			{
+				currentTarget = collider.gameObject;
+			}
 		}
 	}
 }
