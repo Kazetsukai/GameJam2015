316ca510e9fb3a8d59cad3512ea1a7518a56d7e6
joejimufu <righteouslyalgebraic@gmail.com>
2015-01-25 15:21:05 +1300

    cancvas

---
+++ Hystorical/Assets/Scripts/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	public Canvas gameovercanvas;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		int playerCount = GameObject.FindGameObjectsWithTag("Player").Length;
+		Debug.Log("there are " + playerCount + " players alive");
+
+		if (playerCount <= 0)
+		{
+			try{
+				GameObject muzak = GameObject.Find("Muzak");
+				muzak.GetComponent<AudioSource>().Stop();	
+			}
+			catch{}
+
+			gameovercanvas.transform.parent = Camera.main.transform;
+			gameovercanvas.enabled = true;
+		}
+	}
+}
